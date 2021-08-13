5cbdc26eb3d31e1c96e8e9165165d5ac5d19f65c
YuriiTrofimenko <tyaa@ukr.net>
2021-08-13 19:33:21 +0300

    else, switch

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 // console.log(1 * 2)
-if (2 * 2 === 4) {
+const x = 5
+/* if (2 * 2 === x) {
   console.log('2 * 2 === 4 is true')
+} else {
+  console.log(`2 * 2 === ${x} is false`)
+} */
+
+/* if (x === 1) {
+  console.log('One')
+} else if (x === 2) {
+  console.log('Two')
+} else if (x === 3) {
+  console.log('Three')
+} else {
+  console.log('Unknown Number')
+} */
+
+switch (x) {
+  case 4:
+    console.log('Four')
+    break;
+  case 5:
+    console.log('Five')
+    break;
+  default:
+    console.log('Unknown Number')
+    break;
 }
